8e59af2672608ae8aadd4adcea05684de1aeef0b
a.kurdyukov <a.kurdyukov@cti.ru>
2019-07-21 23:51:18 +0300

    #implement_external_interaction find out parsing problem

--- lib/elixir_awesome/external/parser.ex
+++ lib/elixir_awesome/external/parser.ex
@@ -10,11 +10,27 @@ defmodule ElixirAwesome.External.Parser do
   """
   @spec perform(binary) :: {:ok, {list(map), list(map)}} | {:error, term}
   def perform(markdown) do
-    with xml <- Cmark.to_xml() do
-
+    with parsed_xml <- markdown_to_parsed_xml(markdown),
+         sections_data <- get_sections_data(parsed_xml),
+         libraries_data <- get_libraries_data(parsed_xml, sections_data) do
+      {:ok, {sections_data, libraries_data}}
     else
       error -> error
     end
-    {:ok, {[], []}}
+  end
+
+  defp markdown_to_parsed_xml(markdown) do
+    markdown
+    |> Cmark.to_xml()
+    |> String.replace("<!DOCTYPE document SYSTEM \"CommonMark.dtd\">\n", "")
+    |> parse()
+  end
+
+  defp get_sections_data(parsed_xml) do
+    []
+  end
+
+  defp get_libraries_data(parsed_xml, sections_data) do
+    []
   end
 end
